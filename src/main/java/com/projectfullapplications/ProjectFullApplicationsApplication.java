11daed3f9ea329fde9da28d362963556ad7d9f2c
Tiagoodfer <tiagodfer@icloud.com>
2024-10-01 17:53:54 -0600

    First commit

---
+++ src/main/java/com/projectfullapplications/ProjectFullApplicationsApplication.java
@@ -0,0 +1,13 @@
+package com.projectfullapplications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFullApplicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFullApplicationsApplication.class, args);
+	}
+
+}
