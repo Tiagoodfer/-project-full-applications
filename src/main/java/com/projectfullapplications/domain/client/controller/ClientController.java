9b0a74bee292954df96dcef6208c5b5d8060550b
Tiagoodfer <tiagodfer@icloud.com>
2024-10-02 09:39:48 -0600

    Client - GET,POST,PUT and DELETE

--- src/main/java/com/projectfullapplications/domain/client/controller/ClientController.java
+++ src/main/java/com/projectfullapplications/domain/client/controller/ClientController.java
@@ -1,4 +1,44 @@
 package com.projectfullapplications.domain.client.controller;
 
+import com.projectfullapplications.domain.client.Client;
+import com.projectfullapplications.domain.client.repository.ClientRepository;
+import com.projectfullapplications.domain.client.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/client")
 public class ClientController {
+
+    @Autowired
+    private ClientService clientService;
+
+    @PostMapping
+    public ResponseEntity<Client> create(@RequestBody Client client) {
+        Client createdClient = clientService.create(client);
+        return new ResponseEntity<>(createdClient, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Client>> findAll() {
+        List<Client> allClients = clientService.findAll();
+        return new ResponseEntity<>(allClients, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Client> deleteById(@PathVariable("id") UUID uuid) {
+        clientService.deleteById(uuid);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Client> update(@PathVariable("id") UUID uuid, @RequestBody Client client) {
+        Client updateClient = clientService.update(uuid, client);
+        return new ResponseEntity<>(updateClient, HttpStatus.NO_CONTENT);
+    }
 }
--- src/main/java/com/projectfullapplications/domain/client/service/ClientService.java
+++ src/main/java/com/projectfullapplications/domain/client/service/ClientService.java
@@ -1,4 +1,38 @@
 package com.projectfullapplications.domain.client.service;
 
+import com.projectfullapplications.domain.client.Client;
+import com.projectfullapplications.domain.client.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
 public class ClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    public Client create(Client client) {
+        clientRepository.save(client);
+        return client;
+    }
+
+    public List<Client> findAll() {
+        return clientRepository.findAll();
+    }
+
+
+    public void deleteById(UUID uuid) {
+        clientRepository.deleteById(uuid);
+    }
+
+    public Client update(UUID uuid, Client updateClient) {
+        Client client = clientRepository.findById(uuid).orElseThrow(() -> new RuntimeException("Client not found"));
+        client.setName(updateClient.getName());
+        client.setEmail(updateClient.getEmail());
+        return clientRepository.save(client);
+    }
 }
